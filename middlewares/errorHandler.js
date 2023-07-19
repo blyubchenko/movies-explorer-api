eac23cb51a408f36759148e1b6f13aa5aa6beebb
Boris Lyubchenko <Blyubchenko@ya.ru>
2023-07-20 02:20:19 +0800

    fix: statusCode

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const { HTTP_SERVER_ERROR, HTTP_BAD_REQUEST } = require('../errors/statusErrors'
 const messages = require('../errors/constantsMessages');
 
 const errorHandler = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { statusCode = HTTP_SERVER_ERROR, message } = err;
   if (message === 'Validation failed') {
     res
       .status(HTTP_BAD_REQUEST)
